8132c895d34aff8b2f9f4c53f5f838823901bc2f
Terry <twang1010@hotmail.com>
2018-05-07 16:11:35 -0400

    parameterized registry decorator, a different way to register instead of using '@' deprecate

--- deco/registry.py
+++ deco/registry.py
@@ -3,14 +3,14 @@ registry = set()
 
 
 def register(active=True):
-    def decorate(func):
+    def decorate1(func):
         print('running register (active=%s)->decorate(%s)' % (active, func))
         if active:
             registry.add(func)
         else:
             registry.discard(func)
         return func
-    return decorate
+    return decorate1
 
 
 @register()
@@ -27,14 +27,25 @@ def f3():
     print('running f3()')
 
 
+def f4():
+    print('running f4()')
+
+
 def main():
     print('running main()')
     print('registry->', registry)
     f1()
     f2()
     f3()
-    print('again gregistry->', registry)
+    print('again registry->', registry)
 
+    #register f4()
+    register()(f4)
+    print('again registry->', registry)
+
+    #de-register f4()
+    register(active=False)(f4)
+    print('again registry->', registry)
 
 if __name__ == '__main__':
     main()
