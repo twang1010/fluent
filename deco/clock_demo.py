f9a8cc9945fb85e377fb59e1d8cdf10610e3cb8b
Terry <twang1010@hotmail.com>
2018-05-07 10:14:54 -0400

    wrapped by functools.wraps to keep the attributes

--- deco/clock_demo.py
+++ deco/clock_demo.py
@@ -1,5 +1,5 @@
 import time
-from clock_deco import clock
+from clock_deco2 import clock
 
 @clock
 def snooze(seconds):
@@ -15,3 +15,5 @@ if __name__=='__main__':
 
     print('*' * 40, 'calling factorial(6)')
     print('6! =' , factorial(6))
+    # function name and dir were blind by decorator, using function.wraps will solve this issue
+    print(factorial.__name__, factorial.__doc__)
