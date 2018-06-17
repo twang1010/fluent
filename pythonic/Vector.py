e1d9864fa5710663c27fffd5c0fc15860690eccc
Terry <twang1010@hotmail.com>
2018-06-17 15:38:43 -0400

    add polar coordination repr

--- pythonic/Vector.py
+++ pythonic/Vector.py
@@ -6,9 +6,9 @@
 (3.0, 5.0)
 >>> v7 = Vector(range(7))
 >>> v7[1:4]
-Vector[[1.0, 2.0, 3.0]]
+Vector([1.0, 2.0, 3.0])
 >>> v7[-1:]
-Vector[[6.0]]
+Vector([6.0])
 >>> v7[1,2]
 Traceback (most recent call last):
     ...
@@ -16,7 +16,7 @@ TypeError: Vector indices must be integers
 
 >>> v= Vector(range(5))
 >>> v
-Vector[[0.0, 1.0, 2.0, 3.0, 4.0]]
+Vector([0.0, 1.0, 2.0, 3.0, 4.0])
 >>> v.x
 0.0
 >>> v.x = 10
@@ -24,10 +24,177 @@ Traceback (most recent call last):
     ...
 AttributeError: readonly attribute 'x'
 >>> v
-Vector[[0.0, 1.0, 2.0, 3.0, 4.0]]
+Vector([0.0, 1.0, 2.0, 3.0, 4.0])
 >>> vv =Vector([0.0, 1.0, 2.0, 3.0, 4.0])
 >>> v == vv
 True
+
+test hyper sphere coordination
+>>> format(Vector([-1, -1, -1, -1]), 'h')
+'<2.0, 2.0943951023931957, 2.186276035465284, 3.9269908169872414>'
+>>> format(Vector([2, 2, 2, 2]), '.3eh')
+'<4.000e+00, 1.047e+00, 9.553e-01, 7.854e-01>'
+>>> format(Vector([0, 1, 0, 0]), '0.5fh')
+'<1.00000, 1.57080, 0.00000, 0.00000>'
+
+A multidimensional ``Vector`` class, take 5
+A ``Vector`` is built from an iterable of numbers::
+>>> Vector([3.1, 4.2])
+Vector([3.1, 4.2])
+>>> Vector((3, 4, 5))
+Vector([3.0, 4.0, 5.0])
+>>> Vector(range(10))
+Vector([0.0, 1.0, 2.0, 3.0, 4.0, ...])
+
+Tests with two dimensions (same results as ``vector2d_v1.py``)::
+>>> v1 = Vector([3, 4])
+>>> x, y = v1
+>>> x, y
+(3.0, 4.0)
+>>> v1
+Vector([3.0, 4.0])
+>>> v1_clone = eval(repr(v1))
+>>> v1 == v1_clone
+True
+>>> print(v1)
+(3.0, 4.0)
+>>> octets = bytes(v1)
+>>> octets
+b'd\\x00\\x00\\x00\\x00\\x00\\x00\\x08@\\x00\\x00\\x00\\x00\\x00\\x00\\x10@'
+>>> abs(v1)
+5.0
+>>> bool(v1), bool(Vector([0, 0]))
+(True, False)
+
+Test of ``.frombytes()`` class method:
+>>> v1_clone = Vector.frombytes(bytes(v1))
+>>> v1_clone
+Vector([3.0, 4.0])
+>>> v1 == v1_clone
+True
+
+Tests with three dimensions::
+>>> v1 = Vector([3, 4, 5])
+>>> x, y, z = v1
+>>> x, y, z
+(3.0, 4.0, 5.0)
+>>> v1
+Vector([3.0, 4.0, 5.0])
+>>> v1_clone = eval(repr(v1))
+>>> v1 == v1_clone
+True
+>>> print(v1)
+(3.0, 4.0, 5.0)
+>>> abs(v1) # doctest:+ELLIPSIS
+7.071067811...
+>>> bool(v1), bool(Vector([0, 0, 0]))
+(True, False)
+
+Tests with many dimensions::
+>>> v7 = Vector(range(7))
+>>> v7
+Vector([0.0, 1.0, 2.0, 3.0, 4.0, ...])
+>>> abs(v7) # doctest:+ELLIPSIS
+9.53939201...
+
+Test of ``.__bytes__`` and ``.frombytes()`` methods::
+>>> v1 = Vector([3, 4, 5])
+>>> v1_clone = Vector.frombytes(bytes(v1))
+>>> v1_clone
+Vector([3.0, 4.0, 5.0])
+>>> v1 == v1_clone
+True
+
+Tests of sequence behavior::
+>>> v1 = Vector([3, 4, 5])
+>>> len(v1)
+3
+>>> v1[0], v1[len(v1)-1], v1[-1]
+(3.0, 5.0, 5.0)
+
+Test of slicing::
+>>> v7 = Vector(range(7))
+>>> v7[-1]
+6.0
+>>> v7[1:4]
+Vector([1.0, 2.0, 3.0])
+>>> v7[-1:]
+Vector([6.0])
+>>> v7[1,2]
+Traceback (most recent call last):
+...
+TypeError: Vector indices must be integers
+
+Tests of dynamic attribute access::
+>>> v7 = Vector(range(10))
+>>> v7.x
+0.0
+>>> v7.y, v7.z, v7.t
+(1.0, 2.0, 3.0)
+
+Dynamic attribute lookup failures::
+>>> v7.k
+Traceback (most recent call last):
+...
+AttributeError: 'Vector' object has no attribute 'k'
+>>> v3 = Vector(range(3))
+>>> v3.t
+Traceback (most recent call last):
+...
+AttributeError: 'Vector' object has no attribute 't'
+>>> v3.spam
+Traceback (most recent call last):
+...
+AttributeError: 'Vector' object has no attribute 'spam'
+
+Tests of hashing::
+>>> v1 = Vector([3, 4])
+>>> v2 = Vector([3.1, 4.2])
+>>> v3 = Vector([3, 4, 5])
+>>> v6 = Vector(range(6))
+>>> hash(v1), hash(v3), hash(v6)
+(7, 2, 1)
+
+Most hash values of non-integers vary from a 32-bit to 64-bit CPython build::
+>>> import sys
+>>> hash(v2) == (384307168202284039 if sys.maxsize > 2**32 else 357915986)
+True
+
+Tests of ``format()`` with Cartesian coordinates in 2D::
+>>> v1 = Vector([3, 4])
+>>> format(v1)
+'(3.0, 4.0)'
+>>> format(v1, '.2f')
+'(3.00, 4.00)'
+>>> format(v1, '.3e')
+'(3.000e+00, 4.000e+00)'
+
+Tests of ``format()`` with Cartesian coordinates in 3D and 7D::
+>>> v3 = Vector([3, 4, 5])
+>>> format(v3)
+'(3.0, 4.0, 5.0)'
+>>> format(Vector(range(7)))
+'(0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0)'
+
+Tests of ``format()`` with spherical coordinates in 2D, 3D and 4D::
+>>> format(Vector([1, 1]), 'h') # doctest:+ELLIPSIS
+'<1.414213..., 0.785398...>'
+>>> format(Vector([1, 1]), '.3eh')
+'<1.414e+00, 7.854e-01>'
+>>> format(Vector([1, 1]), '0.5fh')
+'<1.41421, 0.78540>'
+>>> format(Vector([1, 1, 1]), 'h') # doctest:+ELLIPSIS
+'<1.73205..., 0.95531..., 0.78539...>'
+>>> format(Vector([2, 2, 2]), '.3eh')
+'<3.464e+00, 9.553e-01, 7.854e-01>'
+>>> format(Vector([0, 0, 0]), '0.5fh')
+'<0.00000, 0.00000, 0.00000>'
+>>> format(Vector([-1, -1, -1, -1]), 'h') # doctest:+ELLIPSIS
+'<2.0, 2.09439..., 2.18627..., 3.92699...>'
+>>> format(Vector([2, 2, 2, 2]), '.3eh')
+'<4.000e+00, 1.047e+00, 9.553e-01, 7.854e-01>'
+>>> format(Vector([0, 1, 0, 0]), '0.5fh')
+'<1.00000, 1.57080, 0.00000, 0.00000>'
 """
 
 from array import array
@@ -36,6 +203,8 @@ import operator
 import reprlib
 import math
 import numbers
+import itertools
+
 
 class Vector(object):
     typecode = 'd'
@@ -49,7 +218,7 @@ class Vector(object):
     def __repr__(self):
         components = reprlib.repr(self._components)
         components = components[components.find('['):-1]
-        return 'Vector[{}]'.format(components)
+        return 'Vector({})'.format(components)
 
     def __str__(self):
         return str(tuple(self))
@@ -84,12 +253,6 @@ class Vector(object):
 
     def __bool__(self):
         return abs(self) != 0
-
-    @classmethod
-    def frombytes(cls, octets):
-        typecode = chr(octets[0])
-        memv = memoryview(octets[1:])
-        return cls(memv)
 
     def __len__(self):
         return len(self._components)
@@ -132,3 +295,31 @@ class Vector(object):
     def __hash__(self):
         hashs = map(hash, self._components)
         return functools.reduce(operator.xor, hashs, 0)
+
+    def angle(self, n):
+        r = math.sqrt(sum(x*x for x in self[n:]))
+        a = math.atan2(r, self[n-1])
+        if (n == len(self) - 1) and (self[-1] < 0):
+            return math.pi*2 -a
+        else:
+            return a
+
+    def angles(self):
+        return(self.angle(n) for n in range(1, len(self)))
+
+    def __format__(self, format_spec=''):
+        if format_spec.endswith('h'): #hyper-sphere coord
+            format_spec = format_spec[:-1]
+            coords = itertools.chain([abs(self)], self.angles())
+            outer_fmt = '<{}>'
+        else:
+            coords = self
+            outer_fmt = '({})'
+        components = (format(c, format_spec) for c in coords)
+        return outer_fmt.format(', '.join(components))
+
+    @classmethod
+    def frombytes(cls, octets):
+        typecode = chr(octets[0])
+        memv = memoryview(octets[1:]).cast(typecode)
+        return cls(memv)
